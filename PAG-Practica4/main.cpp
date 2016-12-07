bc3a915f54b5d2d934446d3acb96278f62f2f0ff
ManuJGQ <manueljgq@gmail.com>
2016-12-07 11:47:33 +0100

    Cambios menores

--- PAG-Practica4/main.cpp
+++ PAG-Practica4/main.cpp
@@ -1,10 +1,6 @@
 #include "PagRenderer.h"
 #include "PagCamera.h"
 
-#include <GL/glew.h> //glew SIEMPRE va antes del glfw
-#include <GLFW/glfw3.h>
-#include "gtc\matrix_transform.hpp"
-
 PagRenderer escena;
 PagCamera camera;
 
